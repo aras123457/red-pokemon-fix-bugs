bf39ee147ad2536c7a095cc5bd0bb23389372487
ARAS AKGÜL <94735664+aras123457@users.noreply.github.com>
2022-02-12 16:40:11 +0300

    Add files via upload

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -900,7 +900,7 @@ class PlayState extends MusicBeatState
 		iconP2.visible = !ClientPrefs.hideHud;
 		add(iconP2);
 		reloadHealthBarColors();
-                 var creditTxt:FlxText = new FlxText(4,healthBarBG.y + 20,0,("Port by (Aras Akgül) "), 24);
+                 var creditTxt:FlxText = new FlxText(4,healthBarBG.y + 20,0,("Port by Aras Akgül "), 24);
         creditTxt.scrollFactor.set();
         creditTxt.setFormat("VCR OSD Mono", 24, FlxColor.WHITE, LEFT, FlxTextBorderStyle.OUTLINE, FlxColor.BLACK);
         creditTxt.borderColor = FlxColor.BLACK;
